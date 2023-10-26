f4171a60c10bc2cb513dbaac6e880f7d3117bcd7
Jon Cooper <joncoop@users.noreply.github.com>
2023-10-26 14:02:26 -0400

    Refactored to make Game object

--- game.py
+++ game.py
@@ -3,19 +3,14 @@ import pygame
 import entities
 import math
 
-# Screen settings
+# Initialize game engine
+pygame.init()
+
+# Settings
 WIDTH = 800
 HEIGHT = 600
 TITLE = "Moving Out"
 FPS = 60
-
-# Initialize game engine
-pygame.init()
-
-# Make the screen
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-pygame.display.set_caption(TITLE)
-clock = pygame.time.Clock()
 
 # Colors
 BLACK = (0, 0, 0)
@@ -37,163 +32,170 @@ PLAYING = 1
 WIN = 2
 LOSE = 3
 
-# Helper functions
-def new_game():
-    global p1, g, players, items, obstacles, goal, current_scene, time_remaining
 
-    players = pygame.sprite.Group()
-    items = pygame.sprite.Group()
-    obstacles = pygame.sprite.Group()
-    goal = pygame.sprite.GroupSingle()
+class Game:
 
-    # Make game objects
-    p1 = entities.Player([700, 525, 50, 50], RED)
-    players.add(p1)
+    def __init__(self):
 
-    wall1 = entities.Obstacle([100, 200, 400, 25], WHITE)
-    wall2 = entities.Obstacle([100, 225, 25, 275], WHITE)
-    wall3 = entities.Obstacle([475, 225, 25, 275], WHITE)
-    wall4 = entities.Obstacle([100, 500, 100, 25], WHITE)
-    wall5 = entities.Obstacle([400, 500, 100, 25], WHITE)
-    tree = entities.Obstacle([400, 50, 50, 50], GREEN)
-    obstacles.add(wall1, wall2, wall3, wall4, wall5, tree)
+        self.screen = pygame.display.set_mode([WIDTH, HEIGHT])
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
 
-    g = entities.Goal([600, 250, 150, 200], GRAY)
-    goal.add(g)
+        self.running = True
+        self.new_game()
 
-    i1 = entities.Item([400, 300, 25, 25], YELLOW)
-    i2 = entities.Item([100, 50, 25, 25], YELLOW)
-    i3 = entities.Item([200, 400, 25, 25], YELLOW)
-    items.add(i1, i2, i3)
+    def new_game(self):
+        self.players = pygame.sprite.Group()
+        self.items = pygame.sprite.Group()
+        self.obstacles = pygame.sprite.Group()
+        self.goal = pygame.sprite.GroupSingle()
 
-    current_scene = START
-    time_remaining = 30 * FPS
+        # Make game objects
+        self.p1 = entities.Player([700, 525, 50, 50], RED)
+        self.players.add(self.p1)
 
-def start():
-    global current_scene
-    current_scene = PLAYING
+        wall1 = entities.Obstacle([100, 200, 400, 25], WHITE)
+        wall2 = entities.Obstacle([100, 225, 25, 275], WHITE)
+        wall3 = entities.Obstacle([475, 225, 25, 275], WHITE)
+        wall4 = entities.Obstacle([100, 500, 100, 25], WHITE)
+        wall5 = entities.Obstacle([400, 500, 100, 25], WHITE)
+        tree = entities.Obstacle([400, 50, 50, 50], GREEN)
+        self.obstacles.add(wall1, wall2, wall3, wall4, wall5, tree)
 
-def win():
-    global current_scene
-    current_scene = WIN
+        self.g = entities.Goal([600, 250, 150, 200], GRAY)
+        self.goal.add(self.g)
 
-def lose():
-    global current_scene
-    current_scene = LOSE
+        i1 = entities.Item([400, 300, 25, 25], YELLOW)
+        i2 = entities.Item([100, 50, 25, 25], YELLOW)
+        i3 = entities.Item([200, 400, 25, 25], YELLOW)
+        self.items.add(i1, i2, i3)
 
-def show_start_screen():
-    text = FONT_LG.render(TITLE, True, BLUE)
-    rect = text.get_rect()
-    rect.center = WIDTH // 2, HEIGHT // 2
-    screen.blit(text, rect)
+        self.current_scene = START
+        self.time_remaining = 30 * FPS
 
-def show_win_screen():
-    text = FONT_MD.render("You win!", True, BLUE)
-    rect = text.get_rect()
-    rect.center = WIDTH // 2, HEIGHT // 2
-    screen.blit(text, rect)
+    def start(self):
+        global current_scene
+        self.current_scene = PLAYING
 
-def show_lose_screen():
-    text = FONT_MD.render("Time's up.", True, BLUE)
-    rect = text.get_rect()
-    rect.center = WIDTH // 2, HEIGHT // 2
-    screen.blit(text, rect)
+    def win(self):
+        global current_scene
+        self.current_scene = WIN
 
-def show_stats():
-    min = (math.ceil(time_remaining / FPS)) // 60
-    sec = (math.ceil(time_remaining / FPS)) % 60
+    def lose(self):
+        global current_scene
+        self.current_scene = LOSE
 
-    text = FONT_SM.render(f"{min}:{sec:02d}", True, BLUE)
-    rect = text.get_rect()
-    rect.topleft = 16, 16
-    screen.blit(text, rect)
+    def show_start_screen(self):
+        text = FONT_LG.render(TITLE, True, BLUE)
+        rect = text.get_rect()
+        rect.center = WIDTH // 2, HEIGHT // 2
+        self.screen.blit(text, rect)
 
-def all_items_in_goal():
-    for item in items:
-        if item.vx != 0 or item.vy != 0:
-            return False
-        
-    items_in_goal = pygame.sprite.spritecollide(g, items, False)
-    held_items = p1.my_item
+    def show_win_screen(self):
+        text = FONT_MD.render("You win!", True, BLUE)
+        rect = text.get_rect()
+        rect.center = WIDTH // 2, HEIGHT // 2
+        self.screen.blit(text, rect)
 
-    return held_items is None and len(items_in_goal) == len(items)
+    def show_lose_screen(self):
+        text = FONT_MD.render("Time's up.", True, BLUE)
+        rect = text.get_rect()
+        rect.center = WIDTH // 2, HEIGHT // 2
+        self.screen.blit(text, rect)
+
+    def show_stats(self):
+        min = (math.ceil(self.time_remaining / FPS)) // 60
+        sec = (math.ceil(self.time_remaining / FPS)) % 60
+
+        text = FONT_SM.render(f"{min}:{sec:02d}", True, BLUE)
+        rect = text.get_rect()
+        rect.topleft = 16, 16
+        self.screen.blit(text, rect)
+
+    def all_items_in_goal(self):
+        for item in self.items:
+            if item.vx != 0 or item.vy != 0:
+                return False
+            
+        items_in_goal = pygame.sprite.spritecollide(self.g, self.items, False)
+        held_items = self.p1.my_item
+
+        return held_items is None and len(items_in_goal) == len(self.items)
+
+    def process_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if self.current_scene == START:
+                    if event.key == pygame.K_SPACE:
+                        self.start()
+                elif self.current_scene == PLAYING:
+                    if event.key == pygame.K_SPACE:
+                        if self.p1.my_item == None:
+                            self.p1.pick_up(self.items)
+                        else:
+                            self.p1.drop()
+                elif self.current_scene in [WIN, LOSE]:
+                    if event.key == pygame.K_r:
+                        self.new_game()
+
+        pressed = pygame.key.get_pressed()
+
+        if pressed[pygame.K_LEFT]:
+            self.p1.go_left()
+        elif pressed[pygame.K_RIGHT]:
+             self.p1.go_right()
+        else:
+             self.p1.stop_x()
+            
+        if pressed[pygame.K_UP]:
+             self.p1.go_up()
+        elif pressed[pygame.K_DOWN]:
+             self.p1.go_down()
+        else:
+             self.p1.stop_y()
+
+    def update(self):
+        if self.current_scene == PLAYING:
+            self.players.update(WIDTH, HEIGHT, self.obstacles, self.items)
+            self.items.update(WIDTH, HEIGHT, self.obstacles)
+            
+            if self.all_items_in_goal():
+                self.win()
+            elif self.time_remaining == 0:
+                self.lose()
+            else:
+                self.time_remaining -= 1
+
+    def render(self):
+        self.screen.fill(BLACK)
+        self.obstacles.draw(self.screen)
+        self.goal.draw(self.screen)
+        self.players.draw(self.screen)
+        self.items.draw(self.screen)
+        self.show_stats()
+
+        if self.current_scene == START:
+            self.show_start_screen()
+        elif self.current_scene == WIN:
+            self.show_win_screen()
+        elif self.current_scene == LOSE:
+            self.show_lose_screen()
+    
+    def run(self):
+        while self.running:
+            self.process_input()
+            self.update()
+            self.render()
+
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+        pygame.quit()
 
 
 # Let's do this!
-new_game()
-
-# Game loop
-running = True
-
-while running:
-    # Input
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if current_scene == START:
-                if event.key == pygame.K_SPACE:
-                    start()
-            elif current_scene == PLAYING:
-                if event.key == pygame.K_SPACE:
-                    if p1.my_item == None:
-                        p1.pick_up(items)
-                    else:
-                        p1.drop()
-            elif current_scene in [WIN, LOSE]:
-                if event.key == pygame.K_r:
-                    new_game()
-
-    pressed = pygame.key.get_pressed()
-
-    if pressed[pygame.K_LEFT]:
-        p1.go_left()
-    elif pressed[pygame.K_RIGHT]:
-        p1.go_right()
-    else:
-        p1.stop_x()
-        
-    if pressed[pygame.K_UP]:
-        p1.go_up()
-    elif pressed[pygame.K_DOWN]:
-        p1.go_down()
-    else:
-        p1.stop_y()
-
-
-    # Logic
-    if current_scene == PLAYING:
-        players.update(WIDTH, HEIGHT, obstacles, items)
-        items.update(WIDTH, HEIGHT, obstacles)
-        
-        if all_items_in_goal():
-            win()
-        elif time_remaining == 0:
-            lose()
-        else:
-            time_remaining -= 1
-
-
-
-    # Drawing
-    screen.fill(BLACK)
-    obstacles.draw(screen)
-    goal.draw(screen)
-    players.draw(screen)
-    items.draw(screen)
-    show_stats()
-
-    if current_scene == START:
-        show_start_screen()
-    elif current_scene == WIN:
-        show_win_screen()
-    elif current_scene == LOSE:
-        show_lose_screen()
-
-    # Update screen
-    pygame.display.update()
-    clock.tick(FPS)
-
-
-# Close window and quit
-pygame.quit()
+if __name__ == '__main__':
+    g = Game()
+    g.run()
